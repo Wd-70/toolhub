c5aab1b98d0f4646f52b0f77e52f5706bd449e01
Wd-70 <kjk7052@naver.com>
2025-05-25 03:15:28 +0900

    사이드바 너비 수정

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -21,9 +21,9 @@ import {
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-const SIDEBAR_WIDTH = "10rem";
+const SIDEBAR_WIDTH = "12rem";
 const SIDEBAR_WIDTH_MOBILE = "12rem";
-const SIDEBAR_WIDTH_ICON = "1rem";
+const SIDEBAR_WIDTH_ICON = "3rem";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 
 type SidebarContext = {
@@ -145,6 +145,7 @@ const SidebarProvider = React.forwardRef<
               className
             )}
             ref={ref}
+            data-sidebar-wrapper="true"
             {...props}
           >
             {children}
@@ -184,6 +185,9 @@ const Sidebar = React.forwardRef<
             "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground",
             className
           )}
+          style={{
+            width: "var(--sidebar-width)",
+          }}
           ref={ref}
           {...props}
         >
@@ -231,6 +235,9 @@ const Sidebar = React.forwardRef<
               ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon))]"
               : "group-data-[collapsible=icon]:w-[--sidebar-width-icon]"
           )}
+          style={{
+            width: "var(--sidebar-width)",
+          }}
         />
         <div
           className={cn(
@@ -244,11 +251,17 @@ const Sidebar = React.forwardRef<
               : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l",
             className
           )}
+          style={{
+            width: "var(--sidebar-width)",
+          }}
           {...props}
         >
           <div
             data-sidebar="sidebar"
             className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+            style={{
+              width: "var(--sidebar-width)",
+            }}
           >
             {children}
           </div>
@@ -358,7 +371,7 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 w-full", className)}
       {...props}
     />
   );
@@ -373,7 +386,7 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 w-full", className)}
       {...props}
     />
   );
@@ -404,7 +417,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto w-full group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props}
@@ -422,6 +435,9 @@ const SidebarGroup = React.forwardRef<
       ref={ref}
       data-sidebar="group"
       className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
+      style={{
+        width: "var(--sidebar-width)",
+      }}
       {...props}
     />
   );
